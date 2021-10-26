f5c6a92fa381d080c1d6f5dbbf6321bd9f539d95
Eurico F. Pedrosa <efp@ua.pt>
2021-10-26 16:46:32 +0100

    Add LaMa SDM to the benchmark (#2)
    * Add LaMa SDM to the benchmark.
    
    * Turn 3D mode on, it was using the 2D version.

---
+++ benchmark/benchmark_lama.cpp
@@ -0,0 +1,72 @@
+#include <benchmark/benchmark.h>
+#include "benchmark_utils.hpp"
+
+#include <lama/sdm/simple_occupancy_map.h>
+
+#define PATCH_LENGTH 8
+
+static void Lama_Create(benchmark::State& state)
+{
+  auto cloud = ReadCloud();
+
+  for (auto _ : state)
+  {
+    lama::SimpleOccupancyMap grid(VOXEL_RESOLUTION, PATCH_LENGTH, true);
+
+    for (const auto& point : *cloud)
+    {
+      Eigen::Vector3d coord(point.x, point.y, point.z);
+      grid.setOccupied(coord);
+    }
+  }
+}
+
+static void Lama_Update(benchmark::State& state)
+{
+  auto cloud = ReadCloud();
+
+  lama::SimpleOccupancyMap grid(VOXEL_RESOLUTION, PATCH_LENGTH, true);
+
+  for (const auto& point : *cloud)
+  {
+    Eigen::Vector3d coord(point.x, point.y, point.z);
+    grid.setOccupied(coord);
+  }
+
+  for (auto _ : state)
+  {
+    for (const auto& point : *cloud)
+    {
+      Eigen::Vector3d coord(point.x, point.y, point.z);
+      grid.setOccupied(coord);
+    }
+  }
+}
+
+static void Lama_IterateAllCells(benchmark::State& state)
+{
+  auto cloud = ReadCloud();
+
+  lama::SimpleOccupancyMap grid(VOXEL_RESOLUTION, PATCH_LENGTH, true);
+
+  {
+    for (const auto& point : *cloud)
+    {
+      Eigen::Vector3d coord(point.x, point.y, point.z);
+      grid.setOccupied(coord);
+    }
+  }
+
+  long count = 0;
+  for (auto _ : state)
+  {
+    auto visitor = [&](const Eigen::Vector3ui&) { count++; };
+    grid.visit_all_cells(visitor);
+  }
+}
+
+// Register the function as a benchmark
+BENCHMARK(Lama_Create);
+BENCHMARK(Lama_Update);
+BENCHMARK(Lama_IterateAllCells);
+
