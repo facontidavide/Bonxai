3d596b072503a0d49e9e213b165b15a8186d1882
jjd9 <jjdang21@gmail.com>
2023-12-09 12:27:02 -0600

    Fix examples and serialization

--- examples/bonxai_map_playground.cpp
+++ examples/bonxai_map_playground.cpp
@@ -1,26 +1,35 @@
 #include <octomap/OcTree.h>
-#include "bonxai_map/pcl_utils.hpp"
-#include "bonxai_map/pointcloud.hpp"
 #include "pcl/point_types.h"
+
+#include "bonxai_map/probabilistic_map.hpp"
+#include "bonxai_map/pcl_utils.hpp"
 
 int main()
 {
   using namespace Bonxai;
 
-  Point3D testA = Eigen::Vector3d(1,2,3);
-//  Point3D testB = pcl::PointXYZ(1,2,3);
+  // constants
+  const double voxel_res = 0.02;
+  const double inv_resolution = 1.0 / voxel_res;
+  const Point3D test_point = Eigen::Vector3d{0.25, 0.12, 1.6};
 
-  std::vector<CoordT> points;
-  ComputeRay({0, 0, 0}, {0.25, 0.12, 1.6}, 0.02, points);
+  // run bonxai
+  std::vector<Bonxai::CoordT> bonxai_ray;
+  const auto coord = Bonxai::PosToCoord(test_point, inv_resolution);
+  ComputeRay({0, 0, 0}, coord, bonxai_ray);
+  std::cout << "points bonxai: " << bonxai_ray.size() << std::endl;
+
+  std::vector<Point3D> points;
+  for(const auto& ray_coord : bonxai_ray){
+    points.push_back(CoordToPos(ray_coord, voxel_res));
+  }
   WritePointsFromPCD("point_to_point.pcd", points);
 
-  std::cout << "points bonxai: " << points.size() << std::endl;
-
-  octomap::OcTree octree(0.02);
+  // run octree
+  octomap::OcTree octree(voxel_res);
   octomap::KeyRay ray;
-  octree.computeRayKeys({0,0,0}, {0.25, 0.12, 1.6}, ray);
-
-  std::cout << "points octree: " << points.size() << std::endl;
+  octree.computeRayKeys({0,0,0}, {(float)test_point.x, (float)test_point.y, (float)test_point.z}, ray);
+  std::cout << "points octree: " << ray.size() << std::endl;
 
   return 0;
 }
